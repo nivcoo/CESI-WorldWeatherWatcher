e13315f607682f9c4a402d8802e6a59455f4e76b
nivcoo <nico64800@live.fr>
2020-09-29 17:18:24 +0200

    feat. interrupt button

--- main/main.ino
+++ main/main.ino
@@ -1,48 +1,95 @@
 
 #include "src/project_libs/Config/Config.h"
 #include "src/project_libs/Led/Led.h"
+#include <Wire.h>
+#include <DS1307.h>
 
 byte version = 1;
 
 //0 : Normal, 1 : Eco, 2 : Maintenance, 3 : Config
-int mode = 3;
-
+int mode = 0;
 Config config(version);
+Led leds(8, 9, 1);
+DS1307 clock;
 
 
-Led leds(7, 8, 1);
+const byte buttonPinGreen = 2;
+const byte buttonPinRed = 3;
 
-//ChainableLED leds(7, 8, 1);
+void clickButtonGreenEvent() {
+  Serial.println("click green");
+  long ms = millis();
+  for (;;) {
+    if ((millis() - ms) > (5 * 1000)) {
+      if (digitalRead(buttonPinGreen) == 0)
+        Serial.println("good 5s");
+      break;
+    }
+  }
+}
 
-
+void clickButtonRedEvent() {
+  Serial.println("click red");
+}
 
 void setup()
 {
-   
+
   Serial.begin(9600);
+  clock.begin();
+  //clock.fillByYMD(2020,9,29);
+  //clock.fillByHMS(16,12,20);
+  //clock.fillDayOfWeek(TUE);
+  //clock.setTime();
   config.showValues();
   //config.setValue("LUMINO", 0);
-  
+  pinMode(buttonPinRed, INPUT_PULLUP);
+  pinMode(buttonPinGreen, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPinRed), clickButtonRedEvent, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(buttonPinGreen), clickButtonGreenEvent, CHANGE);
+  showDate();
+
+}
+
+void showDate()
+{
+  clock.getTime();
+  Serial.print(clock.hour, DEC);
+  Serial.print(":");
+  Serial.print(clock.minute, DEC);
+  Serial.print(":");
+  Serial.print(clock.second, DEC);
+  Serial.print("  ");
+  Serial.print(clock.dayOfMonth, DEC);
+  Serial.print("/");
+  Serial.print(clock.month, DEC);
+  Serial.print("/");
+  Serial.print(clock.year + 2000, DEC);
+  Serial.print(" ");
+  Serial.println(" ");
 }
 
 void loop()
 {
 
+  if (mode == 0)
+    leds.color("GREEN");
+
   //leds.color("BLUE", 2);
   //leds.color("RED");
 
-  leds.color("RED", 10, "YELLOW", 3);
+  //leds.color("RED", 10, "YELLOW", 3);
   //delay(300);
 
-  if(mode == 3)
+  if (mode == 3)
     config.waitValues();
- 
+
   /**Serial.print(hue);
     Serial.print(" ");**/
 }
 
 /**void write_String(char index, String data)
-{
+  {
   int _size = data.length();
   int i;
   for (i = 0; i < _size; i++)
@@ -51,10 +98,10 @@ void loop()
   }
   EEPROM.update(index + _size, '\0');
   //EEPROM.commit();
-}
+  }
 
-String read_String(char add)
-{
+  String read_String(char add)
+  {
   int i;
   char data[100];
   int len = 0;
@@ -68,24 +115,24 @@ String read_String(char add)
   }
   data[len] = '\0';
   return String(data);
-}
+  }
 
-void showEEPROM()
-{
+  void showEEPROM()
+  {
   pinMode(13, OUTPUT);
   for (int i = 0 ; i < EEPROM.length() ; i++) {
     Serial.println(EEPROM.read(i));
   }
   digitalWrite(13, HIGH);
-}
-void clearEEPROM()
-{
+  }
+  void clearEEPROM()
+  {
   pinMode(13, OUTPUT);
   for (int i = 0 ; i < EEPROM.length() ; i++) {
     EEPROM.update(i, 0);
   }
   digitalWrite(13, HIGH);
-}**/
+  }**/
 
 
 
